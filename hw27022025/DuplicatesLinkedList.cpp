bc2f7935a34bcbac85b0897a7d930ceac12c81f0
Ekaterina Logutova <logutova.ekaterina@gmail.com>
2025-03-13 22:13:48 +0300

    new solution to two tasks

--- hw27022025/DuplicatesLinkedList.cpp
+++ hw27022025/DuplicatesLinkedList.cpp
@@ -1,43 +1,72 @@
-
 #include <iostream>
 #include <vector>
 
-//O(1) по памяти, O(n) по времени (все элементы перебираем строго один раз)
-//записываем уникальные элементы в начало
-
 using namespace std;
 
-vector<int> remove_duplicate(vector<int> head) {
-    if (head.empty()) return head;
+//определение структуры для узла связного списка
+struct ListNode
+{
+    int val;         //значение узла
+    ListNode *next;
 
-    int write_index = 0; //индекс для записи уникальных элементов
-    int n = head.size();
-    int i = 0;
+    //конструкторы для удобного создания узлов
+    ListNode() : val(0), next(nullptr) {}  
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-    while (i < n) {
-        if (i < n - 1 && head[i] == head[i + 1]) {
-            //пропускаем все дубликаты
-            int current = head[i];
-            while (i < n && head[i] == current) {
-                i++;
-            }
-        } else {
-            //записываем уникальный элемент
-            head[write_index] = head[i];
-            write_index++;
-            i++;
+//функция для печати связного списка
+void print_list(ListNode *head)
+{
+    ListNode *current = head; 
+    while (current != nullptr)
+    {
+        std::cout << current->val;
+        if (current->next != nullptr)
+        {
+            std::cout << " ";
         }
+        current = current->next;
     }
-    //обрезаем массив до write_index
-    head.resize(write_index);
-    return head;
+    std::cout << std::endl;
 }
 
-int main() {
-    vector<int> head = {1,1,1,2,3};
-    vector<int> new_head = remove_duplicate(head);
+ListNode *duplicates(ListNode *head)
+{
+    ListNode dummy(0);  //создаем фиктивный узел для упрощения обработки головы списка
+    ListNode *prev = &dummy;  //указатель на предыдущий узел, начинаем с фиктивного узла
+    dummy.next = head;  //связываем с головой списка
 
-    for (auto i : new_head)
-        cout << i << " ";
+    while (head)
+    {
+        //проверяем, есть ли следующий узел и совпадают ли значения текущего и следующего узлов
+        if (head->next && head->val == head->next->val)
+        {
+            //пропускаем все дубликаты
+            while (head->next && head->val == head->next->val)
+            {
+                head = head->next;  //переходим к следующему узлу
+            }
+            prev->next = head->next;  //убираем дубликаты из списка
+        }
+        else
+        {
+            prev = prev->next;  //переходим к следующему узлу, если дубликатов нет
+        }
+        head = head->next;  //переходим к следующему узлу
+    }
+    return dummy.next;  //возвращаем голову списка без дубликатов
+}
+
+int main()
+{
+    ListNode *a4 = new ListNode(3); // [1,1,1,2,3]
+    ListNode *a3 = new ListNode(2, a4);
+    ListNode *a2 = new ListNode(1, a3);
+    ListNode *a1 = new ListNode(1, a2);
+    ListNode *a0 = new ListNode(1, a1);
+    a0 = duplicates(a0);
+    print_list(a0);
+
     return 0;
 }
--- hw27022025/MergeTwoSortedLists.cpp
+++ hw27022025/MergeTwoSortedLists.cpp
@@ -3,48 +3,75 @@
 
 using namespace std;
 
-vector<int> merge(const vector<int>& vec1, const vector<int>& vec2) {
-    vector<int> result;
-    size_t i = 0, j = 0;
+//определение структуры для узла связного списка
+struct ListNode
+{
+    int val;         //значение узла
+    ListNode *next;
 
-    //проходим два вектора до конца
-    while (i < vec1.size() && j < vec2.size()) {
-        if (vec1[i] < vec2[j]) {
-            result.push_back(vec1[i]);
-            i++;
-        } else {
-            result.push_back(vec2[j]);
-            j++;
+    //конструкторы для удобного создания узлов
+    ListNode() : val(0), next(nullptr) {}  
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+//функция для печати связного списка
+void print_list(ListNode *head)
+{
+    ListNode *current = head; 
+    while (current != nullptr)
+    {
+        std::cout << current->val;
+        if (current->next != nullptr)
+        {
+            std::cout << " ";
         }
+        current = current->next;
     }
-    //если один из векторов закончился, добавляем оставшиеся элементы другого
-    while (i < vec1.size()) {
-        result.push_back(vec1[i]);
-        i++;
-    }
-    while (j < vec2.size()) {
-        result.push_back(vec2[j]);
-        j++;
-    }
-
-    return result;
+    std::cout << std::endl;
 }
 
-int main() {
-    vector<int> vec1 = {1, 2, 4};
-    vector<int> vec2 = {1, 3, 4};
-    vector<int> mergedVec = merge(vec1, vec2);
-    for (int num : mergedVec) {
-        cout << num << " ";
+
+ListNode *merge(ListNode *list1, ListNode *list2)
+{
+    //создаем фиктивный узел для упрощения обработки головы нового списка
+    ListNode dummy(0);
+    //указатель на текущий узел нового списка, начинаем с фиктивного узла
+    ListNode *current = &dummy;
+    while (list1 && list2)
+    {
+        //сравниваем значения текущих узлов list1 и list2
+        if (list1->val < list2->val)
+        {
+            //если значение в list1 меньше, добавляем его в новый список
+            current->next = list1;
+            //переходим к следующему узлу в list1
+            list1 = list1->next;
+        }
+        else
+        {
+            //добавляем его в новый список значение
+            current->next = list2;
+            //переходим к следующему узлу в list2
+            list2 = list2->next;
+        }
+        //переходим к следующему узлу в новом списке
+        current = current->next;
     }
-    cout << endl; //1 1 2 3 4 4
-    vector<int> vec3 = {};
-    vector<int> vec4 = {};
-    vector<int> mergedVec2 = merge(vec3, vec4);
-    for (int num : mergedVec2) {
-        cout << num << " ";
-    }
-    cout << endl;
+    //если один из списков закончился, добавляем оставшиеся узлы из другого списка
+    current->next = list1 ? list1 : list2;
+    //возвращаем голову нового объединенного списка (следующий узел после фиктивного)
+    return dummy.next;
+}
+int main()
+{
+    ListNode *a_12 = new ListNode(5); // [1,3,5], [1,3,6]
+    ListNode *a_11 = new ListNode(3, a_12);
+    ListNode *a_10 = new ListNode(1, a_11);
+    ListNode *a_22 = new ListNode(6);
+    ListNode *a_21 = new ListNode(3, a_22);
+    ListNode *a_20 = new ListNode(1, a_21);
+    print_list(merge(a_10, a_20));
 
     return 0;
 }
